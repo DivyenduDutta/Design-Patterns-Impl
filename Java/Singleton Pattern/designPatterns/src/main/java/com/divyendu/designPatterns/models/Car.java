8ba12ae480e0cac5ecb060bdec3239a312b240d3
DivyenduDutta <connect2divyendu@gmail.com>
2019-01-26 16:34:06 +0530

    added Car model

---
+++ Java/Singleton Pattern/designPatterns/src/main/java/com/divyendu/designPatterns/models/Car.java
@@ -0,0 +1,27 @@
+package com.divyendu.designPatterns.models;
+
+public class Car {
+
+	private int speed;
+	private String model;
+	
+	public Car(int speed, String model) {
+		this.speed = speed;
+		this.model = model;
+	}
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	
+	
+}
