585af57468641fbc9f3a2929f6a44405bea96382
DivyenduDutta <connect2divyendu@gmail.com>
2019-01-28 21:26:43 +0530

    added support for constructor having multiple arguments: used *args

--- Python/Singleton Pattern/Singleton.py
+++ Python/Singleton Pattern/Singleton.py
@@ -5,14 +5,14 @@ class Singleton_API(object):
     '''Do not access _uniqueInnerInstance directly. Access it via getUniqueInnerInstance()'''
    # _uniqueInnerInstance = None
 
-    def __new__(cls, class_name):
+    def __new__(cls, class_name, *args):
         ''' Overriding __new__() to customize object creation '''
         if not hasattr(cls, 'instance'):
             cls.instance = super(Singleton_API, cls).__new__(cls)
         return cls.instance
 
-    def __init__(self, class_name):
-        self._uniqueInnerInstance = class_name()
+    def __init__(self, class_name, *args):
+        self._uniqueInnerInstance = class_name(*args)
 
     def getUniqueInnerInstance(self):
         return self._uniqueInnerInstance 
--- Python/Singleton Pattern/Singleton_Runner.py
+++ Python/Singleton Pattern/Singleton_Runner.py
@@ -3,10 +3,11 @@ from models.Models import Car
 
 def main():
     class_name = Car 
-    uniqueInstance1 = Singleton_API(class_name)
-    uniqueInstance2 = Singleton_API(class_name)
+    uniqueInstance1 = Singleton_API(class_name, 40, "BMW")
+    uniqueInstance2 = Singleton_API(class_name, 50, "Ferrari")
 
     uniqueInstance1.getUniqueInnerInstance().printDetails()
+    uniqueInstance2.getUniqueInnerInstance().printDetails()
 
 if __name__ == "__main__":
     main()
