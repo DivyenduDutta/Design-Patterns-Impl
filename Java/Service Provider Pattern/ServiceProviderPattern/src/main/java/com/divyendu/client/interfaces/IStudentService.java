80fdc52d12cc6b1e399d9edfc0dbdba8ccdc9676
DivyenduDutta <connect2divyendu@gmail.com>
2019-02-10 12:43:33 +0530

    updated docstring

--- Java/Service Provider Pattern/ServiceProviderPattern/src/main/java/com/divyendu/client/interfaces/IStudentService.java
+++ Java/Service Provider Pattern/ServiceProviderPattern/src/main/java/com/divyendu/client/interfaces/IStudentService.java
@@ -5,6 +5,14 @@ import java.util.List;
 import com.divyendu.ServiceProviderPattern.ServiceInterface.IService;
 import com.divyendu.client.models.Student;
 
+/**
+ * Student Service Interface 
+ * Needs to extend IService interface provided by the Service Provider Pattern API
+ * Specify all methods that the Student Service needs to have in here
+ * 
+ * @author divyendu
+ *
+ */
 public interface IStudentService extends IService{
 	public Student getBestStudentAsPerMarks() throws IllegalAccessException;
 	public Student getStudentDetailsAsPerID(int myStudentID) throws IllegalAccessException;
