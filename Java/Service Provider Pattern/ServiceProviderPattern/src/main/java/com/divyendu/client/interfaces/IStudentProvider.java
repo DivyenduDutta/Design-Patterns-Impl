5147fa98ca010dbf1989efef1a78735422bd7c5a
DivyenduDutta <connect2divyendu@gmail.com>
2019-02-10 12:44:39 +0530

    updated docstring

--- Java/Service Provider Pattern/ServiceProviderPattern/src/main/java/com/divyendu/client/interfaces/IStudentProvider.java
+++ Java/Service Provider Pattern/ServiceProviderPattern/src/main/java/com/divyendu/client/interfaces/IStudentProvider.java
@@ -9,6 +9,8 @@ import com.divyendu.client.models.Student;
  * Provider Interface for Student Provider
  * Needs to extend the IServiceProvider interface provided by Service Provider Pattern API
  * 
+ * Specify all methods here which need to be implemented by the Student Provider class
+ * 
  * @author divyendu
  *
  */
