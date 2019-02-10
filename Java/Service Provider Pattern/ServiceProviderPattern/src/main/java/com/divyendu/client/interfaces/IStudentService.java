71792fd68bf26b553c0f874f8139cadd81cbee02
DivyenduDutta <connect2divyendu@gmail.com>
2019-02-10 12:36:20 +0530

    added new methods

--- Java/Service Provider Pattern/ServiceProviderPattern/src/main/java/com/divyendu/client/interfaces/IStudentService.java
+++ Java/Service Provider Pattern/ServiceProviderPattern/src/main/java/com/divyendu/client/interfaces/IStudentService.java
@@ -6,7 +6,7 @@ import com.divyendu.ServiceProviderPattern.ServiceInterface.IService;
 import com.divyendu.client.models.Student;
 
 public interface IStudentService extends IService{
-	public void initialize(List<Student> myStudentList);
 	public Student getBestStudentAsPerMarks() throws IllegalAccessException;
 	public Student getStudentDetailsAsPerID(int myStudentID) throws IllegalAccessException;
+	public List<Student> retrieveStudentList();
 }
